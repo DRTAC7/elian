d1c7a60e7de58a8a0bbe3defb9564d6e6637b76a
DRTAC7 <45444393+DRTAC7@users.noreply.github.com>
2023-08-14 22:36:03 -0500

    Interactive Mode (#1)
    * Trying to add interactive mode
    
    * Add interactive mode

--- elian.pl
+++ elian.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+# Elian Script Converter
+# Version 1.1
 # Written by dirt_hack_seven with the assistance of ChatGPT
 # Very special thanks to C.C. Elian for the thoughtful creation of this lovely alphabet.
 # https://www.github.com/drtac7/elian
@@ -7,18 +9,17 @@
 use strict;
 use warnings;
 use utf8;
+use Term::ReadKey;  # Add the Term::ReadKey module
 
-use Encode;  # Add the Encode module
+use Encode;
 
 use Exporter 'import';
 our @EXPORT_OK = qw(convert_english_to_elian);
 
-sub convert_english_to_elian 
-{
+sub convert_english_to_elian {
     my $english_text = shift;
 
-    my %char_map =
-    (
+    my %char_map = (
         "a" => "𠃍",
         "b" => "コ",
         "c" => "┘",
@@ -48,20 +49,45 @@ sub convert_english_to_elian
         " " => "  "
     );
 
-    if (!defined $english_text || $english_text eq '') {
-        $english_text = _prompt_for_input("Enter English text: ");
-    }
-
     $english_text = lc($english_text);
 
     my @elian_chars = map { $char_map{$_} // $_ } split //, $english_text;
-    my $elian_text = join(' ', @elian_chars);  # Add a space between characters
+    my $elian_text = join(' ', @elian_chars);
 
     return $elian_text;
 }
 
-sub _prompt_for_input 
-{
+sub interactive_mode {
+    ReadMode('cbreak');
+    my $english_input = '';
+    while (1) {
+        my $key = ReadKey(0);
+        last if ord($key) == 3;  # Ctrl-C to exit
+        if ($key eq "\n" || $key eq "\r") {  # Newline or carriage return key
+            print "\n";
+            $english_input = '';  # Clear the input buffer on new line or carriage return
+        } elsif (ord($key) == 127) {  # Backspace key
+            if (length($english_input) > 0) {
+                print "\b \b";  # Move cursor back, overwrite, and move cursor back again
+                chop($english_input);
+            }
+        } else {
+            $english_input .= $key;
+        }
+        my $elian_text = convert_english_to_elian($english_input);
+        print "\r$elian_text";
+    }
+    ReadMode('normal');
+    print "\n";
+}
+
+sub regular_mode {
+    my $english_text = _prompt_for_input("Enter English text: ");
+    my $elian_text = convert_english_to_elian($english_text);
+    print "Elian: $elian_text\n";
+}
+
+sub _prompt_for_input {
     my ($prompt) = @_;
     print $prompt;
     my $input = <STDIN>;
@@ -72,9 +98,10 @@ sub _prompt_for_input
 # Main script starts here
 binmode STDOUT, ":utf8";  # Set terminal output to UTF-8
 
-my $english_text = join(' ', @ARGV);  # Join command-line arguments into a single string
-my $elian_text = convert_english_to_elian($english_text);
-
-print "Elian: $elian_text\n";
+if (@ARGV && $ARGV[0] eq "-i") {
+    interactive_mode();
+} else {
+    regular_mode();
+}
 
 exit;
